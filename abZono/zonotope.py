a26dc10d3071078503dcb6ee7f5445dc3750d628
Fabian Zürker Aguilar <fzuerker@gmail.com>
2023-12-20 20:49:59 +0100

    Remove functions from zontope

--- abZono/zonotope.py
+++ abZono/zonotope.py
@@ -25,29 +25,6 @@ class Zonotope:
         self.center = self.center.to(device)
         self.generators = self.generators.to(device)
 
-    def linear(self, weight: torch.Tensor, bias: torch.Tensor):
-        return Zonotope(F.linear(self.center, weight, bias), F.linear(self.generators, weight))
-
-    def relu(self):
-        # Bei mehreren Durchläufen max min (oder min max) speichern und wiederverwenden
-        l = self.center - self.generators.abs().sum(dim=0)
-        u = self.center + self.generators.abs().sum(dim=0)
-        if torch.all(l > 0):
-            return self
-        elif torch.all(u < 0):
-            return Zonotope(torch.zeros_like(self.center), torch.zeros_like(self.generators))
-        else:
-            where_crossing = torch.bitwise_and(l < 0, u > 0)
-            slope = u / (u - l) * where_crossing
-            new_generators = -slope * l * 0.5
-            return Zonotope(torch.where(where_crossing, self.center * slope + new_generators, self.center),
-                            torch.cat((torch.where(where_crossing, self.generators * slope, self.generators),
-                                       new_generators.unsqueeze(0))))
-
-    def conv2d(self, weight: torch.Tensor, bias: torch.Tensor, stride, padding, dilation, groups):
-        return Zonotope(F.conv2d(self.center, weight, bias, stride, padding, dilation, groups),
-                        F.conv2d(self.generators, weight, None, stride, padding, dilation, groups))
-
     @property
     def slope_threshold(self) -> torch.Tensor:
         return self.upper_bound / (self.upper_bound - self.lower_bound)
