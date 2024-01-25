9460135e10779040151b49a3e3112badc6324c5e
Fabian Zürker Aguilar <fzuerker@gmail.com>
2024-01-25 16:37:07 +0100

    Refactor test_loss_reduction.py to use load_net_and_input_zonotope function

--- test/test_loss_reduction.py
+++ test/test_loss_reduction.py
@@ -1,9 +1,5 @@
 import unittest
-from abZono.network_transformer import transform_network
-from abZono.zonotope import Zonotope
-from abZono.example_vnnlib import get_num_inputs_outputs, read_vnnlib_simple
-from abZono.utils import numpy_dtype_to_pytorch_dtype
-from onnx2torch import convert
+from abZono.__main__ import load_net_and_input_zonotope
 import torch
 import os
 
@@ -19,25 +15,17 @@ class TestLossReduction(unittest.TestCase):
 
     def test_loss_reduction(self):
         # Load the network and input zonotope
-        network = convert(NETWORK_PATH)
-
-        input_size, input_shape, output_size, output_shape, dtype = get_num_inputs_outputs(NETWORK_PATH)
-        dtype = numpy_dtype_to_pytorch_dtype(dtype)
-        spec = read_vnnlib_simple(SPEC_PATH, input_size, output_size)
-        x = Zonotope.from_vnnlib(spec[0][0], input_shape, dtype)
-
-        # Transform the network and propagate the zonotope
-        transform_network(network, optimize_alpha=True)
+        network, x, spec = load_net_and_input_zonotope(NETWORK_PATH, SPEC_PATH, 'cpu')
         y = network(x)
-        initial_loss = y.vnnlib_loss(spec[0][1][0])
+        initial_loss = y.vnnlib_loss(spec)
         optimizer = torch.optim.Adam(network.parameters(), lr=0.01)
         
-        loss = None  # Assign a default value to the 'loss' variable
+        loss = 0  # Assign a default value to the 'loss' variable
         
         for _ in range(10):
             optimizer.zero_grad()
             y = network(x)
-            loss = y.vnnlib_loss(spec[0][1][0])
+            loss = y.vnnlib_loss(spec)
             loss.backward()
             optimizer.step()
             print(loss)
